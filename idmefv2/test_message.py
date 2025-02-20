ccfbfc5377c9f030d917c5685f67b403144d25e1
François Déchelle <fdechelle@teclib.com>
2025-02-20 17:14:02 +0100

    Add unserialize test

--- idmefv2/test_message.py
+++ idmefv2/test_message.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import uuid
-from idmefv2.message import Message
+from idmefv2.message import Message, SerializedMessage
 
 VERSION = '2.D.V03'
 
@@ -53,6 +53,9 @@ def message2():
     ]
     return msg
 
+def json1() -> bytes:
+    return b'{"Version":"2.D.V03","CreateTime":"2021-11-22T14:42:51.881033Z","ID":"09db946e-673e-49af-b4b2-a8cd9da58de6","Analyzer":{"Category":["LOG"],"IP":"127.0.0.1","Model":"generic","Data":["Log"],"Method":["Monitor"],"Name":"foobar"}}'
+
 def test_message1():
     message1().validate()
 
@@ -63,3 +66,8 @@ def test_serialize_message1():
     pelode = message1().serialize("application/json")
     message = Message.unserialize(pelode)
     assert message['Version'] == VERSION
+
+def test_unserialize_json1():
+    pelode = SerializedMessage("application/json", json1())
+    message = Message.unserialize(pelode)
+    assert message['Analyzer']['Category'][0] == 'LOG'
